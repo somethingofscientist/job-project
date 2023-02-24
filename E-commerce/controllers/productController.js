29da3b4e1b753fdd47e1f79deae6c64633006ea3
Shraddhanand <1shraddhanand@gmail.com>
2023-02-24 10:50:26 +0530

    video 18 begin

--- E-commerce/controllers/productController.js
+++ E-commerce/controllers/productController.js
@@ -29,7 +29,7 @@ export const createProductController = async (req, res) => {
       }
 
 
-      const products = new productModel({ ...req.fileds, slug: slugify(name) })
+      const products = new productModel({ ...req.fields, slug: slugify(name) })
       if (photo) {
          products.photo.data = fs.readFileSync(photo.path)
          products.photo.contentType = photo.type
@@ -91,19 +91,84 @@ export const getSingleProductController = async (req, res) => {
    }
 }
 
-// get product photo
+// get photo
 export const productPhotoController = async (req, res) => {
    try {
       const product = await productModel.findById(req.params.pid).select("photo");
-      if(product.photo.data){
+      if (product.photo.data) {
          res.set("Content-type", product.photo.contentType);
          return res.status(200).send(product.photo.data)
+      }
+      else {
+         return res.status(404).send({ msg: "Image not found" })
       }
    }
    catch (error) {
       console.log(error);
       res.status(500).send({
          success: false, error, message: "Error in getting photo",
+      });
+   }
+}
+
+// delete product
+export const productDeleteController = async (req, res) => {
+   try {
+      const product = await productModel.findByIdAndDelete(req.params.pid).select("-photo");
+      return res.status(200).send({
+         success: true,
+         message: 'Product deleted'
+      })
+   }
+   catch (error) {
+      console.log(error);
+      res.status(500).send({
+         success: false, error, message: "Error in deleting product",
+      });
+   }
+}
+
+// updateProductController
+export const updateProductController = async (req, res) => {
+   try {
+      const { name, description, price, category, quantity, shipping } = req.fields;
+      const { photo } = req.files;
+      // alidation
+      switch (true) {
+         case !name:
+            return res.status(500).send({ error: 'Name is required' })
+         case !description:
+            return res.status(500).send({ error: 'description is required' })
+         case !price:
+            return res.status(500).send({ error: 'price is required' })
+         case !category:
+            return res.status(500).send({ error: 'category is required' })
+         case !quantity:
+            return res.status(500).send({ error: 'quantity is required' })
+         case photo && photo.size > 1000000:
+            return res.status(500).send({
+               error: 'photo is required and size will be less than 1 mb'
+            })
+      }
+
+
+      const products = await productModel.findByIdAndUpdate(
+         req.params.pid,
+         { ...req.fields, slug: slugify(name) },
+         { new: true }
+      );
+
+      if (photo) {
+         products.photo.data = fs.readFileSync(photo.path)
+         products.photo.contentType = photo.type
+      }
+      await products.save()
+      res.status(201).send({ success: true, message: 'Product updated', products })
+   }
+   catch (error) {
+      console.log(error);
+      res.status(500).send({
+         success: false, error, message: "Error in updating product",
       });
    }
 }
--- E-commerce/routes/productRoute.js
+++ E-commerce/routes/productRoute.js
@@ -4,6 +4,8 @@ import {
    getProductController, 
    getSingleProductController,
    productPhotoController,
+   productDeleteController,
+   updateProductController,
  } from '../controllers/productController.js';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 import formidable from 'express-formidable';
@@ -12,8 +14,10 @@ const router = express.Router();
 
 // routes
 router.post('/create-product', requireSignIn, isAdmin, formidable(), createProductController)
+router.post('/update-product/:pid', requireSignIn, isAdmin, formidable(), updateProductController)
 router.get('/get-product', getProductController)
 router.get('/get-single-product/:slug', getSingleProductController)
 router.get('/product-photo/:pid', productPhotoController)
+router.delete('/product/:pid', productDeleteController)
 
 export default router;
