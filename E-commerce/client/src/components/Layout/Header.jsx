c61dbdbc5e95314f044cf492ae7c3ac0a2798a35
Shraddhanand <1shraddhanand@gmail.com>
2023-02-25 19:28:09 +0530

    video 24

--- E-commerce/client/src/components/Layout/Header.jsx
+++ E-commerce/client/src/components/Layout/Header.jsx
@@ -4,11 +4,12 @@ import { FcShop } from 'react-icons/fc'
 import { useAuth } from '../../Context/Auth'
 import toast from "react-hot-toast";
 import SearchInput from '../Forms/SearchInput';
-
+import useCategory from '../Hooks/useCategory';
 
 const Header = () => {
 
    const [auth, setAuth] = useAuth();
+   const categories = useCategory();
    const handleLogout = () => {
       setAuth({
          ...auth,
@@ -33,6 +34,33 @@ const Header = () => {
                   <SearchInput />
                   <li className="nav-item">
                      <Link to="/" className="nav-link">Home</Link>
+                  </li>
+
+                  <li className="nav-item dropdown">
+                     <Link
+                        className="nav-link dropdown-toggle"
+                        to={"/categories"}
+                        data-bs-toggle="dropdown"
+                     >
+                        Categories
+                     </Link>
+                     <ul className="dropdown-menu">
+                        <li>
+                           <Link className="dropdown-item" to={"/categories"}>
+                              All Categories
+                           </Link>
+                        </li>
+                        {categories?.map((c) => (
+                           <li>
+                              <Link
+                                 className="dropdown-item"
+                                 to={`/category/${c.slug}`}
+                              >
+                                 {c.name}
+                              </Link>
+                           </li>
+                        ))}
+                     </ul>
                   </li>
 
                   {/* TERNARY OPERTAOR FOR THE REGISTER AND LOGIN SHOW AND UNSHOW */}
