43e2f1ea305c8cb247751a822da3f6d1691a8f7b
Shraddhanand <1shraddhanand@gmail.com>
2023-02-27 10:35:20 +0530

    s

--- Hooks/usestate_sahil/src/App.js
+++ Hooks/usestate_sahil/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(0)
+  
+  let updateCount = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {count} times</h1>
+      {console.log(count, setCount)}
+      <button onClick={updateCount}>Click Me</button>
+    </>
   );
 }
 
