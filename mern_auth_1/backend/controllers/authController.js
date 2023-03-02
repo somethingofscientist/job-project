63f408b724e20127c6119c314e873456cf0555b8
Shraddhanand <1shraddhanand@gmail.com>
2023-03-02 19:18:31 +0530

    login and register backend complete

--- mern_auth_1/backend/controllers/authController.js
+++ mern_auth_1/backend/controllers/authController.js
@@ -1,6 +1,9 @@
 
 import User from '../models/User.js';
-import { hash } from '../helper/authHelper.js';
+import { compare, hash } from '../helper/authHelper.js';
+import JWT from 'jsonwebtoken';
+
+
 
 export const registerController = async (req, res) => {
    try {
@@ -23,6 +26,46 @@ export const registerController = async (req, res) => {
          status: true,
          message: "user created",
          user
+      })
+
+   } catch (error) {
+      console.log(error.message)
+      res.status(500).send({
+         message: 'error in registration',
+         error: error.message
+      });
+   }
+}
+
+export const loginController = async (req, res) => {
+   try {
+      const {email, password} = req.body;
+
+      if(!email || !password){
+         return res.status(400).send('All Fields Are Mandatory :( ')
+      }
+
+      const user = await User.findOne({email});
+      if(!user){
+         return res.status(404).send('Email is not registered')
+      }
+
+      const match = await compare(password, user.password)
+      if(!match){
+         return res.status(404).send('Email or password is wrong')
+      }
+
+      // token
+      const token = await JWT.sign({_id:user._id}, process.env.secret ,{expiresIn: '7d'})
+
+      return res.status(200).send({
+         status: true, 
+         message: "Login in Successful",
+         user: {
+            name: user.name,
+            email: user.email,
+         },
+         token, 
       })
 
    } catch (error) {
--- mern_auth_1/backend/models/User.js
+++ mern_auth_1/backend/models/User.js
@@ -5,6 +5,6 @@ const userSchema = new mongoose.Schema({
    name: { type: String, required: true },
    email: { type: String, required: true },
    password: { type: String, required: true }
-}, { timeStamsps: true })
+}, { timestamps: true })
 
 export default mongoose.model('user', userSchema);
