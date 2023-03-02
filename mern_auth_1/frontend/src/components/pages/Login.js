de7e31a58bc9fb3fff33cf87213e92e2985c7199
Shraddhanand <1shraddhanand@gmail.com>
2023-03-02 23:14:13 +0530

    login and register backend complete

--- mern_auth_1/frontend/src/components/pages/Login.js
+++ mern_auth_1/frontend/src/components/pages/Login.js
@@ -2,28 +2,40 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../App.css';
 
+
+// form
+const handleSubmit = (e) => {
+   e.preventDefault();
+}
 const Login = () => {
    return (
-      <div className='form-1'>
-         <h1>Login</h1>
+      <>
+         <div className='form-1'>
+            <h1>Login</h1>
 
-         <input
-            type="email"
-            placeholder='Email'
-         />
-         <input
-            type="password"
-            placeholder='Password'
-         />
-         <input
-            type="submit"
-            placeholder='submit'
-         />
+            <input
+               type="email"
+               placeholder='Email'
+            />
+            <input
+               type="password"
+               placeholder='Password'
+            />
+            <div className="button-submit">
+               <button
+                  type="submit"
+                  placeholder='submit'
+               >
+                  Login
+               </button>
+            </div>
+         
 
-         <p className='headline-login'>
-            <Link to="/signup">SignUp</Link>
+         <p className='headline-login' onClick={handleSubmit}>
+            <Link to="/signup" >SignUp</Link>
          </p>
-      </div>
+         </div>
+      </>
    )
 }
 
--- mern_auth_1/frontend/src/components/pages/Signup.js
+++ mern_auth_1/frontend/src/components/pages/Signup.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import '../../App.css';
+import axios from 'axios';
 
 const Signup = () => {
+
+   const [name, setName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+   // form 
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(name,email, password)
+   }
+
    return (
       <div className='form-1'>
          <h1>SignUp</h1>
@@ -10,20 +22,35 @@ const Signup = () => {
          <div className="input-box">
             <input
                type="text"
+               value={name}
+               onChange={(e) => setName(e.target.value)}
                placeholder='Name'
+               required
             />
             <input
                type="email"
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
                placeholder='Email'
+               required
             />
             <input
                type="password"
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}
                placeholder='Password'
+               required
             />
-            <input
-               type="submit"
-               placeholder='submit'
-            />
+            <div className="button-submit">
+               <button
+                  type="submit"
+                  placeholder='submit'
+                  onClick={handleSubmit}
+               >
+               Signup
+               </button>
+            </div>
+
          </div>
 
          <p className='headline-login'>
