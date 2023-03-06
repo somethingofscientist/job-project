bb7c4b7b0c2dd83d8d940fc596d2f439337d097e
Shraddhanand <1shraddhanand@gmail.com>
2023-03-06 12:50:17 +0530

    add frontend navbar and featured blog

--- mern_auth_0/client/src/Pages/Register/Register.jsx
+++ mern_auth_0/client/src/Pages/Register/Register.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css';
+import { request } from '../../utils/fetchApi';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (username === '' || email === '' || password === '') {
+      return
+    }
+
+    try {
+      const options = { 'Content-Type': 'application/json' }
+      const data = await request('/auth/register', "POST", options, { username, email, password })
+      console.log(data);
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
-    <div>Register</div>
+    <div className="r-container">
+      <div className="r-wrapper">
+        <h2>Register</h2>
+        <form onSubmit={handleRegister} >
+          <input type="text" placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+          <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+          <button type='submit' >Register</button>
+          <p>Already have an account ? <Link to="/login" >Login</Link> </p>
+        </form>
+      </div>
+    </div>
   )
 }
 
