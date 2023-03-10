8615bd773c33c531610a728583676f54b7850904
Shraddhanand <1shraddhanand@gmail.com>
2023-03-10 11:37:51 +0530

    mapping blogs mui

--- ready_blog/client/src/pages/Blog.js
+++ ready_blog/client/src/pages/Blog.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import BlogCard from '../components/BlogCard'
+
+
 
 export const Blog = () => {
+  const [blogs, setBlogs] = useState([])
+  // get blogs 
+  const getAllBlogs = async () => {
+    try {
+      const { data } = await axios.get('/api/v1/blog/all-blog')
+      if (data?.success) {
+        setBlogs(data?.blogs);
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  useEffect(() => {
+    getAllBlogs();
+  }, [])
+
   return (
-    <div>Blog</div>
+    <>
+      {blogs && blogs.map(blog => (
+        <BlogCard
+          title={blog.title}
+          description={blog.description}
+          image={blog.image}
+        />
+      ))}
+    </>
   )
 }
