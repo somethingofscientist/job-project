1b669a3f3d6b9f7e946de21a36bbd0123ca0e588
Shraddhanand <1shraddhanand@gmail.com>
2023-03-15 18:48:07 +0530

    hello navbar

--- my_imbd_clone/imbd__clone/src/app/components/Navbar.js
+++ my_imbd_clone/imbd__clone/src/app/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import NavbarItems from './NavbarItems'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+   return (
+      <>
+         <div className='flex justify-center dark:bg-gray-600 bg-amber-100 lg:text-lg p-4'>
+            <NavbarItems title={'Trending'} param="fetchTrending" />
+            <NavbarItems title={'Top Rated'} param="fetchTopRated" />
+         </div>
+      </>
+   )
 }
 
 export default Navbar
