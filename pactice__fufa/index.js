732eaf842b21d1bfd9ab0e772997f8fbd21f13e6
Shraddhanand <1shraddhanand@gmail.com>
2023-03-20 13:20:43 +0530

    dsa session

--- pactice__fufa/index.js
+++ pactice__fufa/index.js
@@ -1,23 +1,13 @@
-arr = [5, 2, 7, 1, 0]
+let a = 5;
+let b = 15;
 
-function selectionSort(arr) {
+a = a + b;
+console.log(a)
+b = a - b;
+console.log(b)
+a = a - b;
+console.log(a)
 
-    for (let i = 0; i < arr.length-1; i++){
+[a, b] = [b, a];
 
-        // Finding the minimum element
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++){
-            if (arr[j] < arr[minIndex]) minIndex = j;
-        }
- 
-        // Swapping the minimum element with the first element
-        let temp = arr[minIndex]
-        arr[minIndex] = arr[i]
-        arr[i] = temp
-    }
-
-    return arr
-}
-
-console.log("Before sorting: ", arr)
-console.log("After sorting: ", selectionSort(arr))
+console.log(a, b);
