5912b56e309f43ec876735efdc042b03543e3014
Shraddhanand <1shraddhanand@gmail.com>
2023-03-20 19:38:44 +0530

    blog-mern-app

---
+++ Sahil-blog-mern-application/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">MERN</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://r4.wallpaperflare.com/wallpaper/53/116/913/harry-potter-harry-potter-and-the-prisoner-of-azkaban-ron-weasley-wallpaper-e38a037aa8a52c4dc852bed3a1189211.jpg"
+        alt="harry-potter-book-wallpaper"
+      />
+    </div>
+  );
+}
